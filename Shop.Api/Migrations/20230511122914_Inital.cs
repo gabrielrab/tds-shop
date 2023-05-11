c2877f99ba5d53319050fdb162eabb3595c9c432
Gabriel Rabelo <contato.ideialisa@gmail.com>
2023-05-11 09:57:13 -0300

    feat: migrations

---
+++ Shop.Api/Migrations/20230511122914_Inital.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inital : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", nullable: false),
+                    Position = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeModel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Barcode = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    AvailableQuantity = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductModel", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Total = table.Column<double>(type: "REAL", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EmployeeId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderModel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderModel_EmployeeModel_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "EmployeeModel",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderLineModel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Total = table.Column<double>(type: "REAL", nullable: false),
+                    ProductId = table.Column<int>(type: "INTEGER", nullable: false),
+                    OrderModelId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderLineModel", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderLineModel_OrderModel_OrderModelId",
+                        column: x => x.OrderModelId,
+                        principalTable: "OrderModel",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_OrderLineModel_ProductModel_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "ProductModel",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeModel_Email",
+                table: "EmployeeModel",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLineModel_OrderModelId",
+                table: "OrderLineModel",
+                column: "OrderModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLineModel_ProductId",
+                table: "OrderLineModel",
+                column: "ProductId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderModel_EmployeeId",
+                table: "OrderModel",
+                column: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderLineModel");
+
+            migrationBuilder.DropTable(
+                name: "OrderModel");
+
+            migrationBuilder.DropTable(
+                name: "ProductModel");
+
+            migrationBuilder.DropTable(
+                name: "EmployeeModel");
+        }
+    }
+}
