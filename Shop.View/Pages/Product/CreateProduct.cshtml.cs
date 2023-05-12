7c980f9cc4861721b0bfca868d70f1e3c72e7395
Lissa Takahashi <99764365+lissatakahashi@users.noreply.github.com>
2023-05-12 15:31:16 -0300

    formulario criado

--- Shop.View/Pages/Product/CreateProduct.cshtml.cs
+++ Shop.View/Pages/Product/CreateProduct.cshtml.cs
@@ -1,20 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace Shop.View.Pages.Product
 {
     public class CreateProduct : PageModel
     {
-        private readonly ILogger<CreateProduct> _logger;
+       
 
-        public CreateProduct(ILogger<CreateProduct> logger)
+        
+
+        public CreateProduct()
         {
-            _logger = logger;
+
         }
 
         public void OnGet()
